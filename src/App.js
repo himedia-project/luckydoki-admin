b7b015340e02676e86fefc11fb9e5f9adf6b2aaa
seonggon do <mooh2jj@naver.com>
2025-02-09 22:40:44 +0900

    기본 header 로직 추가가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
       {/* 404 */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
