c74bbf5235e13500d262de47c89d99061b61da3e
seonggon do <mooh2jj@naver.com>
2025-02-15 00:55:25 +0900

    💄 헤더의 아이콘 사용

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
 import { useSelector, useDispatch } from 'react-redux';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import StoreIcon from '@mui/icons-material/Store';
+import PeopleIcon from '@mui/icons-material/People';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import CelebrationIcon from '@mui/icons-material/Celebration';
 
 import { logoutPost } from '../api/loginApi';
 import { logout } from '../app/redux/loginSlice';
@@ -77,9 +84,12 @@ const Header = () => {
                 color: 'white',
                 textDecoration: 'none',
                 fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
               }}
             >
-              대시보드
+              <DashboardIcon /> 대시보드
             </Typography>
             <Typography
               component={Link}
@@ -92,9 +102,12 @@ const Header = () => {
                 color: 'white',
                 textDecoration: 'none',
                 fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
               }}
             >
-              상품
+              <Inventory2Icon /> 상품
             </Typography>
             <Typography
               component={Link}
@@ -107,9 +120,12 @@ const Header = () => {
                 color: 'white',
                 textDecoration: 'none',
                 fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
               }}
             >
-              샵
+              <StoreIcon /> 샵
             </Typography>
             <Typography
               component={Link}
@@ -122,9 +138,12 @@ const Header = () => {
                 color: 'white',
                 textDecoration: 'none',
                 fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
               }}
             >
-              회원
+              <PeopleIcon /> 회원
             </Typography>
             <Typography
               component={Link}
@@ -137,9 +156,12 @@ const Header = () => {
                 color: 'white',
                 textDecoration: 'none',
                 fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
               }}
             >
-              주문
+              <ShoppingCartIcon /> 주문
             </Typography>
             <Typography
               component={Link}
@@ -152,9 +174,12 @@ const Header = () => {
                 color: 'white',
                 textDecoration: 'none',
                 fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
               }}
             >
-              쿠폰
+              <LocalActivityIcon /> 쿠폰
             </Typography>
             <Typography
               component={Link}
@@ -167,9 +192,12 @@ const Header = () => {
                 color: 'white',
                 textDecoration: 'none',
                 fontWeight: 'bold',
+                display: 'flex',
+                alignItems: 'center',
+                gap: 0.5,
               }}
             >
-              이벤트
+              <CelebrationIcon /> 이벤트
             </Typography>
             {email ? (
               <Button
