ecb8ea95814883022a0d1310e50a8f5f20a6eb83
seonggon do <mooh2jj@naver.com>
2025-02-18 01:25:03 +0900

    🐛 redux-persist 라이브러리 적용(새로고침으로 유저정보 날라가는 거 방지)

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,11 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import loginSlice from './loginSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({
+  loginSlice,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  // 여러 리듀서들을 하나로 결합
-  reducer: {
-    loginSlice: loginSlice, // 로그인 상태 관리
-  },
+  reducer: persistedReducer,
+  // redux-persist 관련 직렬화 에러 방지를 위해 serializableCheck 옵션을 비활성화할 수 있습니다.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './app/redux/store';
+import store, { persistor } from './app/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 );
 
